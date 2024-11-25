69702c7341c2807bb9d99b3c86447531647e45d6
Justin Gossett <95954002+xxxcaliber@users.noreply.github.com>
2024-11-24 21:57:17 -0600

    Update config.h

--- config.h
+++ config.h
@@ -8,19 +8,20 @@
 
 // Touchscreen pins
 #define XPT2046_IRQ 36
-#define XPT2046_MOSI 32
-#define XPT2046_MISO 39
-#define XPT2046_CLK 25
+#define XPT2046_MOSI 13
+#define XPT2046_MISO 12
+#define XPT2046_CLK 14
 #define XPT2046_CS 33
+#define TFT_BL 27
 
 // I2C Configuration
 #define SDA_PIN 21
 #define SCL_PIN 22
 
 // PCA9685 Configuration
-#define PCA9685_I2C_ADDRESS 0x40
-#define LED_CHANNEL 0
-#define FAN_CHANNEL 1
-#define MIST_CHANNEL 2
+//#define PCA9685_I2C_ADDRESS 0x40
+//#define LED_CHANNEL 0
+//#define FAN_CHANNEL 1
+//#define MIST_CHANNEL 2
 
 #endif // CONFIG_H
