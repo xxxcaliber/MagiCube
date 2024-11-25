221cf81788e744091e12747ac5d9bc3cbdf9cfe4
Justin Gossett <95954002+xxxcaliber@users.noreply.github.com>
2024-11-24 22:18:14 -0600

    Update LVGL_Arduino.ino

--- arduino/LVGL_Arduino/LVGL_Arduino.ino
+++ arduino/LVGL_Arduino/LVGL_Arduino.ino
@@ -1,11 +1,11 @@
 /*Using LVGL with Arduino requires some extra steps:
  *Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html  */
 
-#include <lvgl.h>
+//#include <lvgl.h>
 
-#if LV_USE_TFT_ESPI
-#include <TFT_eSPI.h>
-#endif
+//#if LV_USE_TFT_ESPI
+//#include <TFT_eSPI.h>
+//#endif
 
 /*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
  *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
@@ -16,26 +16,26 @@
 //#include <demos/lv_demos.h>
 
 /*Set to your screen resolution and rotation*/
-#define TFT_HOR_RES   320
-#define TFT_VER_RES   240
-#define TFT_ROTATION  LV_DISPLAY_ROTATION_0
+//#define TFT_HOR_RES   320
+//#define TFT_VER_RES   240
+//#define TFT_ROTATION  LV_DISPLAY_ROTATION_0
 
 /*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
-#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
-uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+//#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+//uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
-#if LV_USE_LOG != 0
-void my_print( lv_log_level_t level, const char * buf )
-{
-    LV_UNUSED(level);
-    Serial.println(buf);
-    Serial.flush();
-}
-#endif
+//#if LV_USE_LOG != 0
+//void my_print( lv_log_level_t level, const char * buf )
+//{
+  //  LV_UNUSED(level);
+    //Serial.println(buf);
+    //Serial.flush();
+//}
+//#endif
 
 /* LVGL calls it when a rendered image needs to copied to the display*/
-void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
-{
+//void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+//{
     /*Copy `px map` to the `area`*/
 
     /*For example ("my_..." functions needs to be implemented by you)
@@ -47,12 +47,12 @@ void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
      */
 
     /*Call it to tell LVGL you are ready*/
-    lv_display_flush_ready(disp);
-}
+  //  lv_display_flush_ready(disp);
+//}
 
 /*Read the touchpad*/
-void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
-{
+//void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+//{
     /*For example  ("my_..." functions needs to be implemented by you)
     int32_t x, y;
     bool touched = my_get_touch( &x, &y );
@@ -66,49 +66,49 @@ void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
         data->point.y = y;
     }
      */
-}
+//}
 
 /*use Arduinos millis() as tick source*/
-static uint32_t my_tick(void)
-{
-    return millis();
-}
+//static uint32_t my_tick(void)
+//{
+  //  return millis();/
+//}//
 
-void setup()
-{
-    String LVGL_Arduino = "Hello Arduino! ";
-    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+//void setup()
+//{
+  //  String LVGL_Arduino = "Hello Arduino! ";
+    //LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
-    Serial.begin( 115200 );
-    Serial.println( LVGL_Arduino );
+    //Serial.begin( 115200 );
+    //Serial.println( LVGL_Arduino );
 
-    lv_init();
+    //lv_init();
 
     /*Set a tick source so that LVGL will know how much time elapsed. */
-    lv_tick_set_cb(my_tick);
+    //lv_tick_set_cb(my_tick);
 
     /* register print function for debugging */
-#if LV_USE_LOG != 0
-    lv_log_register_print_cb( my_print );
-#endif
+//#if LV_USE_LOG != 0
+  //  lv_log_register_print_cb( my_print );
+//#endif
 
-    lv_display_t * disp;
-#if LV_USE_TFT_ESPI
+  ///  lv_display_t * disp;
+//#if LV_USE_TFT_ESPI
     /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
-    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
-    lv_display_set_rotation(disp, TFT_ROTATION);
+  //  disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+    //lv_display_set_rotation(disp, TFT_ROTATION);
 
-#else
+//#else
     /*Else create a display yourself*/
-    disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
-    lv_display_set_flush_cb(disp, my_disp_flush);
-    lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
-#endif
+  //  disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+    //lv_display_set_flush_cb(disp, my_disp_flush);
+    //lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+//#endif
 
     /*Initialize the (dummy) input device driver*/
-    lv_indev_t * indev = lv_indev_create();
-    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
-    lv_indev_set_read_cb(indev, my_touchpad_read);
+  //  lv_indev_t * indev = lv_indev_create();
+    //lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    //lv_indev_set_read_cb(indev, my_touchpad_read);
 
     /* Create a simple label
      * ---------------------
@@ -129,15 +129,15 @@ void setup()
      lv_demo_widgets();
      */
 
-    lv_obj_t *label = lv_label_create( lv_screen_active() );
-    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
-    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+    //lv_obj_t *label = lv_label_create( lv_screen_active() );
+    //lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    //lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
 
-    Serial.println( "Setup done" );
-}
+    //Serial.println( "Setup done" );
+//}
 
-void loop()
-{
-    lv_timer_handler(); /* let the GUI do its work */
-    delay(5); /* let this time pass */
-}
+//void loop()
+//{
+  //  lv_timer_handler(); /* let the GUI do its work */
+   // delay(5); /* let this time pass */
+//}
